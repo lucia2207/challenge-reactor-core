b1b06eabe04e6932d44df720001bc153935efbab
lucia2207 <ludef15@gmail.com>
2022-03-17 12:45:43 -0300

    Proyecto base

---
+++ src/main/java/co/com/sofka/challengereactorcore/ChallengeReactorCoreApplication.java
@@ -0,0 +1,13 @@
+package co.com.sofka.challengereactorcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeReactorCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeReactorCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/sofka/challengereactorcore/ChallengeReactorCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.challengereactorcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeReactorCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
